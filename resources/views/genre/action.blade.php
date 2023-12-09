a54704e0be6feab94877b55cdde9c4e00b935b43
DavneyRestraDanaz <davney.restra@mhs.itenas.ac.id>
2023-12-09 21:07:07 +0700

    new2 genre masih action rapihin

--- resources/views/genre/action.blade.php
+++ resources/views/genre/action.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.genre')
 
-@section('content')
-    <!-- Konten spesifik untuk genre 'Action' -->
+@section('container')
     <h2>Action Movies</h2>
     <p>Here are some popular action movies:</p>
     <ul>
         <li>Die Hard</li>
         <li>Mad Max: Fury Road</li>
         <li>John Wick</li>
-        <!-- ... -->
     </ul>
 @endsection
