e70016b5bc3d16511b14ab493dc3f295985cc821
bayuramdhan50 <bayuramdhan50@gmail.com>
2023-12-09 21:22:40 +0700

    update partial navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="/">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -11,13 +11,9 @@
                     <a class="nav-link {{ $title === 'Home' ? 'active' : '' }}" aria-current="page"
                         href="/">Home</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">Genre</a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                    </ul>
+                <li class="nav-item {{ $title === 'Genre' ? 'active' : '' }}">
+                    <a class="nav-link" href="/genre">Genre</a>
+                </li>
                 </li>
                 <li class="nav-item {{ $title === 'Community' ? 'active' : '' }}">
                     <a class="nav-link" href="/community">Community</a>
