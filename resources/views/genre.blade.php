7dfb2899374ed91087868ec2383fa1fa99ecf1c7
DavneyRestraDanaz <davney.restra@mhs.itenas.ac.id>
2023-12-12 22:43:18 +0700

    genre tombol

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -4,67 +4,67 @@
 <h1>Genre</h1>
     <div class="row">
         <div class="col-md-3">
-            <a href="/action/" title="Action">Action</a>
+            <a href="/action" class="genre-button" title="Action">Action</a>
         </div>
         <div class="col-md-3">
-            <a href="/adventure/" title="Adventure">Adventure</a>
+            <a href="/adventure/" class="genre-button" title="Adventure">Adventure</a>
         </div>
         <div class="col-md-3">
-            <a href="/animation/" title="Animation">Animation</a>
+            <a href="/animation/" class="genre-button" title="Animation">Animation</a>
         </div>
         <div class="col-md-3">
-            <a href="/anime/" title="Anime">Anime</a>
+            <a href="/anime/" class="genre-button" title="Anime">Anime</a>
         </div>
         <div class="col-md-3">
-            <a href="/biography/" title="Biography">Biography</a>
+            <a href="/biography/" class="genre-button" title="Biography">Biography</a>
         </div>
         <div class="col-md-3">
-            <a href="/comedy/" title="Comedy">Comedy</a>
+            <a href="/comedy/" class="genre-button" title="Comedy">Comedy</a>
         </div>
         <div class="col-md-3">
-            <a href="/crime/" title="Crime">Crime</a>
+            <a href="/crime/" class="genre-button" title="Crime">Crime</a>
         </div>
         <div class="col-md-3">
-            <a href="/documentary/" title="Documentary">Documentary</a>
+            <a href="/documentary/" class="genre-button" title="Documentary">Documentary</a>
         </div>
         <div class="col-md-3">
-            <a href="/drama/" title="Drama">Drama</a>
+            <a href="/drama/" class="genre-button" title="Drama">Drama</a>
         </div>
         <div class="col-md-3">
-            <a href="/family/" title="Family">Family</a>
+            <a href="/family/" class="genre-button" title="Family">Family</a>
         </div>
         <div class="col-md-3">
-            <a href="/fantasy/" title="Fantasy">Fantasy</a>
+            <a href="/fantasy/" class="genre-button" title="Fantasy">Fantasy</a>
         </div>
         <div class="col-md-3">
-            <a href="/history/" title="History">History</a>
+            <a href="/history/" class="genre-button" title="History">History</a>
         </div>
         <div class="col-md-3">
-            <a href="/horror/" title="Horror">Horror</a>
+            <a href="/horror/" class="genre-button" title="Horror">Horror</a>
         </div>
         <div class="col-md-3">
-            <a href="/music/" title="Music">Music</a>
+            <a href="/music/" class="genre-button" title="Music">Music</a>
         </div>
         <div class="col-md-3">
-            <a href="/mystery/" title="Mystery">Mystery</a>
+            <a href="/mystery/" class="genre-button" title="Mystery">Mystery</a>
         </div>
         <div class="col-md-3">
-            <a href="/romance/" title="Romance">Romance</a>
+            <a href="/romance/" class="genre-button" title="Romance">Romance</a>
         </div>
         <div class="col-md-3">
-            <a href="/science-fiction/" title="Science Fiction">Science Fiction</a>
+            <a href="/science-fiction/" class="genre-button" title="Science Fiction">Science Fiction</a>
         </div>
         <div class="col-md-3">
-            <a href="/sport/" title="Sport">Sport</a>
+            <a href="/sport/" class="genre-button" title="Sport">Sport</a>
         </div>
         <div class="col-md-3">
-            <a href="/thriller/" title="Thriller">Thriller</a>
+            <a href="/thriller/" class="genre-button" title="Thriller">Thriller</a>
         </div>
         <div class="col-md-3">
-            <a href="/war/" title="War">War</a>
+            <a href="/war/" class="genre-button" title="War">War</a>
         </div>
         <div class="col-md-3">
-            <a href="/western/" title="Western">Western</a>
+            <a href="/western/" class="genre-button" title="Western">Western</a>
         </div>
     </div>
 @endsection
