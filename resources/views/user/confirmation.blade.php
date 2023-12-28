f56b5033f285c064a4b949a1f0530852cd44d2b6
bayuramdhan50 <bayuramdhan50@gmail.com>
2023-12-28 15:48:05 +0700

    perbaikan untuk tampilan konfirmasi pembayaran

--- resources/views/user/confirmation.blade.php
+++ resources/views/user/confirmation.blade.php
@@ -6,39 +6,75 @@
 
         <!-- Display the confirmation information here -->
         <div>
-            <p>Nama: {{ isset($checkoutData['nama']) ? $checkoutData['nama'] : 'N/A' }}</p>
-            <p>Alamat: {{ isset($checkoutData['alamat']) ? $checkoutData['alamat'] : 'N/A' }}</p>
-            <p>Telepon: {{ isset($checkoutData['telepon']) ? $checkoutData['telepon'] : 'N/A' }}</p>
+            <form>
+                <label for="nama">Nama:</label>
+                <input type="text" id="nama" name="nama"
+                    value="{{ isset($checkoutData['nama']) ? $checkoutData['nama'] : 'N/A' }}" readonly>
 
-            <div>
-                <p>Total Harga: RP {{ $checkoutData['total_harga'] }}</p>
-            </div>
+                <label for="alamat">Alamat:</label>
+                <input type="text" id="alamat" name="alamat"
+                    value="{{ isset($checkoutData['alamat']) ? $checkoutData['alamat'] : 'N/A' }}" readonly>
 
-            <!-- Button Pembayaran -->
-            <button id="pay-now-button" class="btn" type="submit" data-snap-token="{{ $checkoutData->snap_token }}">
-                Bayar Sekarang
-            </button>
+                <label for="telepon">Telepon:</label>
+                <input type="text" id="telepon" name="telepon"
+                    value="{{ isset($checkoutData['telepon']) ? $checkoutData['telepon'] : 'N/A' }}" readonly>
+
+                <div>
+                    <label for="total_harga">Total Harga:</label>
+                    <input type="text" id="total_harga" name="total_harga" value="RP {{ $checkoutData['total_harga'] }}"
+                        readonly>
+                </div>
+
+                <!-- Button Pembayaran -->
+                <button id="pay-now-button" class="btn" type="submit" data-snap-token="{{ $checkoutData->snap_token }}">
+                    Bayar Sekarang
+                </button>
+            </form>
         </div>
-    </div>
-@endsection
+    @endsection
 
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('#pay-now-button').on('click', function() {
-            var snapToken = $(this).data('snap-token');
-            snap.pay(snapToken);
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#pay-now-button').on('click', function() {
+                var snapToken = $(this).data('snap-token');
+                snap.pay(snapToken);
+            });
         });
-    });
-</script>
+    </script>
 
-<style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100%;
-    }
-</style>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            max-width: 400px;
+            margin: auto;
+        }
+
+        label {
+            font-weight: bold;
+        }
+
+        input {
+            padding: 8px;
+            width: 100%;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            outline: none;
+        }
+
+        div {
+            margin-top: 15px;
+        }
+    </style>
