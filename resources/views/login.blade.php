7ea7140169d0f8bd71c2f48c76a7ea864c62515a
bayuramdhan50 <bayuramdhan50@gmail.com>
2023-12-30 01:39:53 +0700

    ubah tampilan regis dan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,54 +1,51 @@
 @extends('layouts.main')
 
 @section('container')
-    <div style="margin-top: 4rem;">
-        @if (session()->has('status'))
-            <div
-                style="background-color: #d4edda; color: #155724; border: 1px solid #c3e6cb; border-radius: .25rem; padding: .75rem 1.25rem; margin-bottom: 1rem;">
-                {{ session('status') }}
-            </div>
-        @endif
-        <main style="max-width: 20rem; margin: auto;">
-            <form action="/login" method="post">
-                @csrf
-                <h1 style="margin-bottom: 1.5rem; text-align: center; font-size: 1.5rem; font-weight: normal;">Please Login
-                </h1>
-                <div style="margin-bottom: 1.5rem;">
-                    <input type="username" name='username'
-                        style="width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .2rem; border: 1px solid #ced4da;"
-                        id="username" placeholder="Username" autofocus required value="{{ old('username') }}">
-                    @error('username')
-                        <div
-                            style="color: #721c24; background-color: #f8d7da; border: 1px solid #f5c6cb; border-radius: .25rem; padding: .75rem 1.25rem; margin-top: .5rem;">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <div style="margin-bottom: 1.5rem;">
-                    <input type="password" name="password"
-                        style="width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .2rem; border: 1px solid #ced4da;"
-                        id="password" placeholder="Password" required>
-                    @error('password')
-                        <div
-                            style="color: #721c24; background-color: #f8d7da; border: 1px solid #f5c6cb; border-radius: .25rem; padding: .75rem 1.25rem; margin-top: .5rem;">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div style="margin-bottom: 1.5rem;">
-                    <div style="display: flex; align-items: center;">
-                        <input type="checkbox" class="form-check-input" style="margin-right: .25rem;" value="remember-me"
-                            id="flexCheckDefault">
-                        <label for="flexCheckDefault" style="margin-bottom: 0;">Remember me</label>
+    @if (session()->has('status'))
+        <div
+            style="background-color: #d4edda; color: #155724; border: 1px solid #c3e6cb; border-radius: .25rem; padding: .75rem 1.25rem; margin-bottom: 1rem;">
+            {{ session('status') }}
+        </div>
+    @endif
+    <section class="seclog">
+        <div class="form-box">
+            <div class="form-value">
+                <form action="/login" method="post">
+                    @csrf
+                    <h2 class="login">Login</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="username" name='username' id="username" autofocus required
+                            value="{{ old('username') }}">
+                        <label for="">Username</label>
+                        @error('username')
+                            <div>
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                </div>
-                <button
-                    style="width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .2rem; border: none; color: #fff; background-color: #007bff; cursor: pointer;"
-                    type="submit">Login</button>
-            </form>
-            <small style="display: block; text-align: center; margin-top: .75rem;">Don't have an account? <a
-                    href="/register" style="color: #007bff; text-decoration: none;">Make an account now!</a></small>
-        </main>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" name="password" id="password" required>
+                        <label for="">Password</label>
+                        @error('password')
+                            <div>
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="forget">
+
+                        <label for="flexCheckDefault"><input type="checkbox" class="form-check-input" value="remember-me"
+                                id="flexCheckDefault">Remember me</label>
+                    </div>
+                    <button type="submit" class="btn-login">Login</button>
+                    <div class="register">
+                        <p>Don't have a account <a href="/register">Register</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
     </div>
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,51 +1,49 @@
 @extends('layouts.main')
 
 @section('container')
-    <div style="margin-top: 4rem;">
-        <main style="max-width: 20rem; margin: auto;">
-            <form action="/register" method="post">
-                @csrf
-                <h1 style="margin-bottom: 1.5rem; text-align: center; font-size: 1.5rem; font-weight: normal;">Registration
-                    Form</h1>
-                <div style="margin-bottom: 1.5rem;">
-                    <input type="text" name='username'
-                        style="width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .2rem; border: 1px solid #ced4da;"
-                        id="username" placeholder="Username" required value="{{ old('username') }}">
-                    @error('username')
-                        <div
-                            style="color: #721c24; background-color: #f8d7da; border: 1px solid #f5c6cb; border-radius: .25rem; padding: .75rem 1.25rem; margin-top: .5rem;">
-                            {{ $message }}
-                        </div>
-                    @enderror
+    <section class="seclog">
+        <div class="form-box">
+            <div class="form-value">
+                <form action="/register" method="post">
+                    @csrf
+                    <h2 class="login">Registration</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="username" name='username' id="username" autofocus required
+                            value="{{ old('username') }}">
+                        <label for="">Username</label>
+                        @error('username')
+                            <div>
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" name='email' id="email" required value="{{ old('email') }}">
+                        <label for="">Email</label>
+                        @error('email')
+                            <div>
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" name="password" id="password" required>
+                        <label for="">Password</label>
+                        @error('password')
+                            <div>
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn-login">Register</button>
+                </form>
+                <div class="register">
+                    <p>Already Registered? <a href="/login">Login now!</a></p>
                 </div>
-                <div style="margin-bottom: 1.5rem;">
-                    <input type="email" name='email'
-                        style="width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .2rem; border: 1px solid #ced4da;"
-                        id="email" placeholder="name@example.com" required value="{{ old('email') }}">
-                    @error('email')
-                        <div
-                            style="color: #721c24; background-color: #f8d7da; border: 1px solid #f5c6cb; border-radius: .25rem; padding: .75rem 1.25rem; margin-top: .5rem;">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <div style="margin-bottom: 1.5rem;">
-                    <input type="password" name='password'
-                        style="width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .2rem; border: 1px solid #ced4da;"
-                        id="password" placeholder="Password" required value="{{ old('password') }}">
-                    @error('password')
-                        <div
-                            style="color: #721c24; background-color: #f8d7da; border: 1px solid #f5c6cb; border-radius: .25rem; padding: .75rem 1.25rem; margin-top: .5rem;">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <button
-                    style="width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .2rem; border: none; color: #fff; background-color: #007bff; cursor: pointer;"
-                    type="submit">Register</button>
-            </form>
-            <small style="display: block; text-align: center; margin-top: .75rem;">Already Registered? <a href="/login"
-                    style="color: #007bff; text-decoration: none;">Login now!</a></small>
-        </main>
-    </div>
+            </div>
+        </div>
+    </section>
 @endsection
