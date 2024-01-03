1234b77180b0da71682af1f8ba486e5904a6c42d
DavneyRestraDanaz <davney.restra@mhs.itenas.ac.id>
2024-01-04 01:36:06 +0700

    hapus judul genre

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <div class="container">
         <div class="button-row">
-            <h1 class="genre">Genre</h1>
+            <h1 class="genre"></h1>
             <div class="button-container">
                 @foreach ($genres as $id => $name)
                     <a href="{{ route('genres.show', ['name' => $name]) }}" class="genre-button">{{ $name }}</a>
